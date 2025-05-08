8083b436faf42f9618c80694d8263b290f6ed681
rermrf <3219431643@qq.com>
2025-05-08 22:17:30 +0800

    test

--- hello.go
+++ hello.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	//使用 http 包创建一个6060端口的http服务
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, world!")
+		fmt.Fprintln(w, "Hello, world! 测试CI/CD")
 	})
 	http.ListenAndServe(":6060", nil)
 }
